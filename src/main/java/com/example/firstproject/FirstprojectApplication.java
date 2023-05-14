3889e29e40b526f7f39878e3f8b56aa342c208d0
yunddorri <ys.2871@daum.net>
2023-05-15 01:40:54 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//컨트롤러 선언
+@Controller
+public class FirstController {
+    @GetMapping("/hi")//접속할 url 입력
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "동글");
+        return "greetings"; //templates/greetings.mustache -> 브라우저로 전송!
+    }
+}
