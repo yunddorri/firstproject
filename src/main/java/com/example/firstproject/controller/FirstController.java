150cb650998bc0f0884c1f75323fcfee709ad72e
yunddorri <ys.2871@daum.net>
2023-05-15 20:58:30 +0900

    폼 데이터 주고 받기까지 완료

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -12,4 +12,10 @@ public class FirstController {
         model.addAttribute("username", "동글");
         return "greetings"; //templates/greetings.mustache -> 브라우저로 전송!
     }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname", "둥글");
+        return "goodbye";
+    }
 }
